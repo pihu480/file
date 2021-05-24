1469b66446768154b31634568a0721eed86f54d9
pihu480 <84699440+pihu480@users.noreply.github.com>
2021-05-24 12:10:42 +0530

    Add files via upload

---
+++ count line.py
@@ -0,0 +1,6 @@
+my_filr5=open("ex.txt")
+count=0
+for i in my_filr5:
+    count=count+1
+print(count)
+my_filr5.close()
---
+++ read a file.py
@@ -0,0 +1,3 @@
+my_filr4=open("ex.txt","r")
+print(my_filr4.read())
+my_filr4.close()
